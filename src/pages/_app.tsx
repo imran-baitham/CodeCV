866899baa8af08ed554df0df83afe8b5964bccc6
imran-baitham <imranbaitham0001@gmail.com>
2022-09-14 20:17:20 +0500

    completed hydrated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,27 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <Hydrated>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Hydrated>
   );
 }
 
 export default MyApp;
+
+const Hydrated = ({ children }: { children?: React.ReactNode }) => {
+  const [hydration, setHydration] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setHydration(true);
+    }
+  }, []);
+  return hydration ? children : <div></div>;
+};
